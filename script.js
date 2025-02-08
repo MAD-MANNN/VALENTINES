8ee01e204ab9c854f57ba264d7234571bf8e73c3
Renz Gerlou Kristian S. Jimenez <jimenezrenzkristian@gmail.com>
2025-02-08 17:20:22 +0800

    Add files via upload
    FIRST COMMIT

---
+++ script.js
@@ -0,0 +1,13 @@
+
+no = document.getElementById("noBT");
+
+function evt(){
+    document.getElementById("yesBT").style.fontSize = "15px";
+}
+
+
+if(document.getElementById("noBT")!=document.getElementById("yesBT")){
+    document.getElementById("yesBT").style.fontSize = "30px";
+}
+
+
